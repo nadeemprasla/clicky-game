6c65c364289b21bf4bdbe4b4912ce5c8f5406bdb
Nadeem Prasla <nadeemprasla@yahoo.com>
2019-12-18 22:31:13 -0600

    Updates

---
+++ precache-manifest.fc4362d533c5f0ed8014e36d33d2e909.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3760d60090925b14b5563cf54d571fe3",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fae3deb643b5b31ddae7",
+    "url": "/clicky-game/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a79c74f495abbfdd49ea",
+    "url": "/clicky-game/static/css/main.41322e0e.chunk.css"
+  },
+  {
+    "revision": "fae3deb643b5b31ddae7",
+    "url": "/clicky-game/static/js/2.14e5a34f.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/clicky-game/static/js/2.14e5a34f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a79c74f495abbfdd49ea",
+    "url": "/clicky-game/static/js/main.e0cf4af8.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  },
+  {
+    "revision": "0ec8f47977aeb67c459a119a95598c95",
+    "url": "/clicky-game/static/media/Texture-Background.0ec8f479.jpg"
+  }
+]);
